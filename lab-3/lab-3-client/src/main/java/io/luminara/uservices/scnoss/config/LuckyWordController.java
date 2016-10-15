7dea2e0004d321d4ca924e91ecdbc752a4ca68c6
ofadeyi <ofadeyi@gmail.com>
2016-10-15 19:35:38 +0100

    Part2: done

---
+++ lab-3/lab-3-client/src/main/java/io/luminara/uservices/scnoss/config/LuckyWordController.java
@@ -0,0 +1,19 @@
+package io.luminara.uservices.scnoss.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ofadeyi.
+ */
+@RestController
+public class LuckyWordController {
+
+    @Value("${lucky-word}") String luckyWord;
+
+    @RequestMapping("/lucky-word")
+    public String showLuckyWord() {
+        return "The lucky word is: " + luckyWord;
+    }
+}
