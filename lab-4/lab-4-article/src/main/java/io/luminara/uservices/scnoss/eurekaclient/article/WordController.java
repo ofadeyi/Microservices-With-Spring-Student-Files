77c99fed2f808963b8216f033ad8421439fe8041
ofadeyi <ofadeyi@gmail.com>
2016-10-16 00:27:27 +0100

    Part2: done

---
+++ lab-4/lab-4-article/src/main/java/io/luminara/uservices/scnoss/eurekaclient/article/WordController.java
@@ -0,0 +1,22 @@
+package io.luminara.uservices.scnoss.eurekaclient.article;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ofadeyi.
+ */
+@RestController
+public class WordController {
+    @Value("${words}")
+    String words;
+
+    @RequestMapping("/")
+    public @ResponseBody String getWord() {
+        String[] wordArray = words.split(",");
+        int i = (int)Math.round(Math.random() * (wordArray.length - 1));
+        return wordArray[i];
+    }
+}
